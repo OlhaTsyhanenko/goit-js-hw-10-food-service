57f2575d3ee387433929ca199bd7d27c17fa740d
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-08-02 23:56:16 +0300

    добавление шаблона

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './sass/main.scss';
+
+//import './sass/main.scss';
+import cardTpl from './templates/card.hbs';
+import menu from './menu.json'
+
+const menuContainer = document.querySelector('.js-menu');
+const cardsMarkup = createCardsMenu(menu);
+
+menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createCardsMenu(menu) {
+    return menu.map(cardTpl).join('');
+}
